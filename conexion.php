e23fec9f05bcb7c3b0c78cbd27ae03144303776b
javier teheran magallanez <141957021+jatemast@users.noreply.github.com>
2025-01-23 22:55:35 -0500

    Update conexion.php
    datos confidenciales aquí de conexión de base de datos.

--- conexion.php
+++ conexion.php
@@ -1,15 +1,1 @@
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "proyectotaller";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
-?>
+ 
